bf231706cb45b53a4d6632667f236d64cd209f4c
jaeiljoo <sgsgself@naver.com>
2021-12-31 00:02:16 +0900

    [챕터11]

---
+++ udemy/Chatper11/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react"
+import AvailableMeals from "./AvailableMeals"
+import MealsSummray from "./MealsSummary"
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummray />
+      <AvailableMeals />
+    </>
+  )
+}
+
+export default Meals
