087ffdc310b1bd5b0075f2b64b372d1baf1ddff1
jaeiljoo <sgsgself@naver.com>
2022-01-07 00:51:45 +0900

    타스연습3

--- 코딩공부/TypeScript/toyproject/src/App.tsx
+++ 코딩공부/TypeScript/toyproject/src/App.tsx
@@ -1,15 +1,28 @@
 import "./App.css"
-import CounterContainer from "./container/CounterContainer"
-import Inputs from "./Inputs"
-import ReducerSample from "./ReducerSample"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import Detail from "./pages/Detail"
+import Add from "./pages/Add"
+import Edit from "./pages/Edit"
+import NotFound from "./pages/NotFound"
+import { ErrorBoundary } from "react-error-boundary"
+import Error from "./pages/Error"
 
 function App() {
   return (
-    <div>
-      <Inputs />
-      <ReducerSample />
-      <CounterContainer />
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   )
 }
 
--- 코딩공부/TypeScript/toyproject/src/index.tsx
+++ 코딩공부/TypeScript/toyproject/src/index.tsx
@@ -2,17 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import { createStore } from "redux"
-import rootReducer from "./modules"
-import { Provider } from "react-redux"
 
-const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 )
