695b7a98a3569b3d1ba2ead7cd098f2cd593380c
jaeiljoo <sgsgself@naver.com>
2022-01-17 23:54:46 +0900

    리액트 타스 복습

--- 코딩공부/TypeScript/react-ts/src/App.tsx
+++ 코딩공부/TypeScript/react-ts/src/App.tsx
@@ -1,32 +1,8 @@
-import React, { useState } from "react"
+import React from "react"
 import "./App.css"
-import NewTodo from "./components/NewTodo"
-import Todos from "./components/Todos"
-import Todo from "./models/todo"
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
-
-  const onAddTodo = (enteredText: string) => {
-    const newTodos = new Todo(enteredText)
-
-    setTodos((prevTodos) => {
-      return prevTodos!.concat(newTodos)
-    })
-  }
-
-  const removeToHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId)
-    })
-  }
-
-  return (
-    <div>
-      <NewTodo onAddTodo={onAddTodo} />
-      <Todos items={todos} onRemoveTodo={removeToHandler} />
-    </div>
-  )
+  return <div>gdggd</div>
 }
 
 export default App
