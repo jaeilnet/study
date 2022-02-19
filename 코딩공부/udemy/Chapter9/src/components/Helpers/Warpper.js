926090f2bed668b48f700fe1e54baa4373585dbb
jaeiljoo <sgsgself@naver.com>
2022-02-20 00:58:09 +0900

    파일구조 변경 완료

--- 코딩공부/udemy/Chapter9/src/components/Helpers/Warpper.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Warpper = (props) => {
-  return props.children
-}
-
-export default Warpper
--- 코딩공부/udemy/TypeScript/react-reduxsaga-typescript/src/redux/modeuls/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects"
-import { authSaga } from "./auth"
-
-// 루트 사가 all 안에 하위 사가들을 추가
-export default function* rootSaga() {
-  yield all([authSaga()])
-}
