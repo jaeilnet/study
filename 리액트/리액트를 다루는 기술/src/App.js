e54168f361f1701755b90dd8a993068ca89a1b38
jaeiljoo <sgsgself@naver.com>
2022-02-23 23:44:45 +0900

    리액트를 다루는 기술 투두 프로젝트

--- 리액트/리액트를 다루는 기술/src/App.js
+++ 리액트/리액트를 다루는 기술/src/App.js
@@ -1,36 +1,68 @@
-import React ,{ Component } from 'react';
-import './App.css';
-import Info from "./component/Info"
-import LifeCylceSample from "./component/LifeCylceSample"
-import UseCallbackSample from "./component/UseCallbackSample"
-import UseMemoSample from "./component/UseMemoSample"
+import React, { useCallback, useRef, useState } from "react"
+import TodoInsert from "./component/TodoInsert"
+import TodoList from "./component/TodoList"
+import TodoTemplate from "./component/TodoTemplate"
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() + 16777215).toString(16)
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어보기",
+      checked: true,
+    },
+  ])
+
+  const nextId = useRef(4)
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      }
+      setTodos(todos.concat(todo))
+      nextId.current += 1
+    },
+    [todos]
+  )
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+    },
+    [todos]
+  )
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      )
+    },
+    [todos]
+  )
+
+  return (
+    <div>
+      <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </TodoTemplate>
+    </div>
+  )
 }
 
-export class App extends Component {
-  state = {
-    color: "#000",
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.handleClick}>랜덤색상</button>
-        <LifeCylceSample color={this.state.color} />
-        <UseMemoSample />
-        <UseCallbackSample />
-        <Info />
-      </div>
-    )
-  }
-}
-
-export default App;
+export default App
