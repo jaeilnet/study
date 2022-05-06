92f1578f1d2d860fcbd369d3909b6bc5c8f94bce
jaeilnet <sgsgself@naver.com>
2022-05-06 20:09:39 +0900

    알고리즘 비밀지도

--- 알고리즘/Lev1/index.js
+++ 알고리즘/Lev1/index.js
@@ -1,5 +1,23 @@
-function solution(s) {
-  const index = Math.floor(s.length / 2);
+function solution(n, arr1, arr2) {
+  var answer = [];
 
-  return (answer = s.length % 2 === 0 ? s[index - 1] + s[index] : s[index]);
+  const num = arr1.map((e) => e.toString(2).padStart(n, 0));
+  const num2 = arr2.map((e) => e.toString(2).padStart(n, 0));
+
+  let sum = 0;
+  let ary = [];
+  let arr = [];
+
+  for (let i = 0; i < num.length; i++) {
+    arr = [];
+    ary = [];
+    for (let j = 0; j < num.length; j++) {
+      sum = Number(num[i][j]) + Number(num2[i][j]);
+      arr.push(sum);
+      ary.push(arr[j] > 0 ? "#" : " ");
+    }
+    answer.push(ary.join("").split(",").join(""));
+  }
+
+  return answer;
 }
