e1366713cd2aa6452614867d0ac8c0e25e3b9520
jaeiljoo <sgsgself@naver.com>
2022-09-16 23:06:34 +0900

    리팩토링

---
+++ 리팩토링/index.js
@@ -0,0 +1,79 @@
+const data = {
+  invocides: {
+    customer: "BigCo",
+    performance: [
+      {
+        playId: "hamlet",
+        audience: 55,
+      },
+      {
+        playId: "as-like",
+        audience: 35,
+      },
+      {
+        playId: "othello",
+        audience: 40,
+      },
+    ],
+  },
+
+  plays: {
+    hamlet: { name: "hamlet", type: "tragedy" },
+    "as-like": { name: "As You Like It", type: "comedy" },
+    othello: { name: "Othello", type: "tragedy" },
+  },
+};
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let pref of invoice.performance) {
+    const play = plays[pref.playId];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 4000;
+        if (pref.audience > 30) {
+          thisAmount += 1000 * (pref.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (pref.audience > 20) {
+          thisAmount += 1000 + 500 * (pref.audience - 20);
+        }
+        thisAmount += 300 * pref.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 :${play.type}`);
+    }
+
+    volumeCredits += Math.max(pref.audience - 30, 0);
+
+    if ("comedy" === play.type) volumeCredits += Math.floor(pref.audience / 5);
+
+    result += `${play.name} : ${format(thisAmount / 100)} (${
+      pref.audience
+    }석)\n`;
+
+    totalAmount += thisAmount;
+  }
+
+  result += `총액 ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트 : ${volumeCredits}점 \n`;
+
+  return result;
+}
+
+// console.log(data.invocides.customer);
+console.log(statement(data.invocides, data.plays));
--- 클린코드/index.js
+++ 클린코드/index.js
@@ -1,5 +1,17 @@
-let arr1 = [];
+// console.log(sum(3, 4), "1"); // TypeError: sum is not a function
 
-arr1.length = 3;
+var sum = function (a, b) {
+  return a + b;
+};
 
-console.log(arr1);
+var a = sum(1, 2);
+
+console.log(a, "1"); // 3
+
+var sum = function (x, y) {
+  return x + y + y;
+};
+
+var c = sum(1, 2);
+
+console.log(c, "2"); // 5
