8ceaff6b83985456ca76c74a296e694ce6bc3f4a
jaeilnet <sgsgself@naver.com>
2023-06-01 16:30:01 +0900

    ci test

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -9,7 +9,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Buttons = ({ label, color, ...props }: Props) => {
   return (
-    <button style={{ padding: "12px"}}  className={buttons[color]} {...props}>
+    <button className={buttons[color]} {...props}>
       {label}
     </button>
   );
