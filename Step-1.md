5b573e2f6a77a70e3a2f75b9630da6225e4cc0f2
jaeil.joo <sgsgself@naver.com>
2023-07-06 21:37:14 +0900

    bundle js 생성

--- Step-1.md
+++ Step-1.md
@@ -1,12 +1,12 @@
 # design_patterns
 
-1. 웹팩 설치
+### 1. 웹팩 설치
 
 ```
 npm install webpack webpack-cli --save-dev
 ```
 
-2. webpack config 설정
+### 2. webpack config 설정
 
 ```javascript
 // webpack.config.js;
@@ -37,14 +37,26 @@ module.exports = {
 };
 ```
 
-3. 타입스크립트 설치
+### 3. 타입스크립트 설치
 
 ```
 npm install typescript ts-loader --save-dev
 ```
 
-3. ts-config 생성
+### 4. ts-config 생성
 
 ```
 tsc --init
+```
+
+### 5. html 만들기
+
+```
+- dist 하위 폴더에 index.html 작성 후 bundle.js 를 읽어올 스크립트 파일 작성.
+```
+
+### 6. package.json script 명령어 추가
+
+```
+"bundle": "webpack"
 ```
---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("first");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQSxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZGVzaWduX3BhdHRlcm5zLy4vc3JjL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiZmlyc3RcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
