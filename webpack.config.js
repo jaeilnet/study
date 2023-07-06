ea2118f0340231910cba5186c61cf0ad17c005cb
jaeil.joo <sgsgself@naver.com>
2023-07-06 21:29:46 +0900

    webpack config 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,3 @@
-# design_patterns
-
-1. 웹팩 설치
-
-```
-npm install webpack webpack-cli --save-dev
-```
-
-2. webpack config 설정
-
-```javascript
-// webpack.config.js;
-
 const path = require("path");
 
 module.exports = {
@@ -35,16 +22,3 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 };
-```
-
-3. 타입스크립트 설치
-
-```
-npm install typescript ts-loader --save-dev
-```
-
-3. ts-config 생성
-
-```
-tsc --init
-```
